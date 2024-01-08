451d2b90a02647df7ba17dce13d3888f35c2317f
mabalder <ma_baga@hotmail.com>
2024-01-07 23:15:14 -0600

    Javascript first classes

---
+++ javascript/hola_mundo/js/01-hola-mundo.js
@@ -0,0 +1,7 @@
+
+alert("Hola Mundo con JS")
+alert("Bienvenido al curso")
+
+//document.write("Hola mundo desde un fichero externo")
+//console.log("Muestra esto en la consola")
+//console.log(84+12)
---
+++ javascript/hola_mundo/js/02-variables.js
@@ -0,0 +1,12 @@
+'use strict'
+//VARIABLES
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+var pais_y_continente = pais + ' ' + continente;
+
+pais = "Mexico"
+continente = "Latinoamerica"
+
+console.log(pais, continente, antiguedad)
+alert(pais_y_continente)
