39cbf0ee24ea0aee0e3a173bcf48f061a2a4b3f4
MANUEL ALEJANDRO BALDERRABANO GARCÍA <manuelbg@MANUELs-MacBook-Air.local>
2024-09-15 14:53:03 -0600

    Added 5kyu simple pig latin exercise

---
+++ codewars_exercises/5kyu_Simple_pig_latin.py
@@ -0,0 +1,17 @@
+'''
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+'''
+
+def pig_it(text):
+    return ' '.join([ word[1:] + word[0] + 'ay' if word.isalpha() else word for word in text.split() ])
+
+test = 'Pig latin is cool'
+answer = 'igPay atinlay siay oolcay'
+pig_it(test)
+
+assert pig_it(test) == answer
---
+++ selenium_course/game_automation_project/game_flows.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.service import Service
+import time
+
+##get from configfile
+email = "ma_baga@outlook.com"
+password = 'mabg271195nn'
+
+service_obj = Service()
+chrome_options_obj = Options()
+chrome_options_obj.add_argument("--incognito")
+driver = webdriver.Chrome( service = service_obj, options = chrome_options_obj)
+wait = WebDriverWait(driver, 10)
+
+driver.implicitly_wait(5)
+driver.maximize_window()
+
+driver.get("https://www.nutaku.net/games/aeons-echo/play")
+wait.until(EC.presence_of_element_located((By.XPATH, "//header//span[text()='Login']")))
+driver.find_element(By.XPATH, "//header//span[text()='Login']").click()
+wait.until(EC.presence_of_element_located((By.XPATH,"//div[@class='modal-box signup-login rounded js-modal-box open']")))
+
+driver.find_element(By.XPATH, "//input[@name='email']").send_keys(email)
+driver.find_element(By.XPATH, "//div[@class='flx content-login js-content-login']//input[@name='password']").send_keys(password)
+driver.find_element(By.XPATH, "//button/span[text()='Login']").click()
+
+time.sleep(3)
+wait.until(EC.presence_of_element_located((By.XPATH, "//button[@type='submit']")))
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+
+
+time.sleep(10)
+driver.close()
+
